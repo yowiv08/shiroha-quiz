37676dba89f10b4492875608a4add1f73389044a
REiQEr Local <reiqer-local@users.noreply.local>
2026-05-17 19:23:10 +0800

    feat: ImportScreen 交互优化，版本号更新至 v0.4.3

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         create("native") {
             dimension = "variant"
             applicationId = "com.reqir.shirohaquiz"
-            versionCode = 117
+            versionCode = 119
             versionName = "0.4.3"
         }
     }
