0e4b9733e7d267d6031c8560f85487dd58a15f84
REiQEr Local <reiqer-local@users.noreply.local>
2026-05-20 12:19:38 +0800

    chore: 版本号修正回 v0.4.8

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         create("native") {
             dimension = "variant"
             applicationId = "com.reqir.shirohaquiz"
-            versionCode = 127
+            versionCode = 126
             versionName = "0.4.9"
         }
     }
