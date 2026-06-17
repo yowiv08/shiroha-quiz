6e63c23ae5295a1c2c8575e24792e56ae8bdf299
REiQEr Local <reiqer-local@users.noreply.local>
2026-06-17 20:56:46 +0800

    chore: 版本号 Native v0.8.5

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         create("native") {
             dimension = "variant"
             applicationId = "com.reqir.shirohaquiz"
-            versionCode = 177
-            versionName = "0.8.4"
+            versionCode = 178
+            versionName = "0.8.5"
         }
     }
 
